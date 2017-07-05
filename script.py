4afd0a64c934c7c920ff83fcbed96af7245ec469
rishabh-malik <rishabhmalik249@gmail.com>
2017-07-05 12:06:58 +0530

    adjusting the prog for user input

--- script.py
+++ script.py
@@ -3,8 +3,12 @@ import pandas
 
 #reading data from text file using pandas
 df=pandas.read_csv("Volcanoes-USA.txt")
+
+avr_lat=df['LAT'].mean()
+avr_lon=df['LON'].mean()
+
 #creating a map 
-map=folium.Map(location=[45.372,-121.697],zoom_start=4,tiles='Stamen Terrain')
+map=folium.Map(location=[avr_lat,avr_lon],zoom_start=4,tiles='Stamen Terrain')
 
 #adding marker to map
 for lat,lon,name in zip(df['LAT'], df['LON'], df['NAME']):
