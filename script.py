120151876c14e3addaefda92e7bf413e6503f119
rishabh-malik <rishabhmalik249@gmail.com>
2017-07-05 11:52:23 +0530

    adding markers to map from csv data

--- script.py
+++ script.py
@@ -1,8 +1,15 @@
 import folium
+import pandas
+
+#reading data from text file using pandas
+df=pandas.read_csv("Volcanoes-USA.txt")
 #creating a map 
-map=folium.Map(location=[45,-121],zoom_start=12)
+map=folium.Map(location=[45.372,-121.697],zoom_start=4,tiles='Stamen Terrain')
+
 #adding marker to map
-map.add_child(folium.Marker(location=[45.32,-121.66],popup='Mt hood'))
-map.add_child(folium.Marker(location=[44,-120.66],popup='Mt hood'))
+for lat,lon,name in zip(df['LAT'], df['LON'], df['NAME']):
+ map.add_child(folium.Marker(location=[lat,lon],popup=name))
+
+
 #saving the map as html file
 map.save(outfile='test.html',close_file=True)
