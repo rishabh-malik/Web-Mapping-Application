80db03565cd7cda8d1bd2b3f93e437c706d0ea4b
rishabh-malik <rishabhmalik249@gmail.com>
2017-07-05 11:38:53 +0530

    adding marker to map

--- script.py
+++ script.py
@@ -1,5 +1,8 @@
 import folium
 #creating a map 
 map=folium.Map(location=[45,-121],zoom_start=12)
+#adding marker to map
+map.add_child(folium.Marker(location=[45.32,-121.66],popup='Mt hood'))
+map.add_child(folium.Marker(location=[44,-120.66],popup='Mt hood'))
 #saving the map as html file
 map.save(outfile='test.html',close_file=True)
